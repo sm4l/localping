d985e4ee7319047cfba4c559af35e0233cd14741
Eng Lucas Mendes <86418689+sm4l@users.noreply.github.com>
2024-04-26 16:32:47 -0300

    Create localping.py

---
+++ localping.py
@@ -0,0 +1,51 @@
+import scapy.all as scapy
+import json
+import re
+import paho.mqtt.client as mqtt
+import time
+
+def load_mac_vendors():
+    with open("mac-vendors-export.json", "r") as f:
+        return json.load(f)
+
+def get_mac_vendor(mac, mac_vendors):
+    mac_prefix = mac.replace(":", "").upper()[:6]  # Pegando os primeiros 6 caracteres do MAC
+    for entry in mac_vendors:
+        if mac_prefix.startswith(entry["macPrefix"].replace(":", "").upper()):
+            return entry["vendorName"]
+    return "Desconhecido"
+
+
+def scan_network(network):
+    mac_vendors = load_mac_vendors()
+    arp_request = scapy.ARP(pdst=network)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    devices_list = []
+    for index, element in enumerate(answered_list):
+        device_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        device_dict["vendor"] = get_mac_vendor(device_dict["mac"], mac_vendors)
+        devices_list.append(device_dict)
+    return devices_list
+
+def publish_mqtt(devices_list):
+    client = mqtt.Client()
+    client.connect("aerisiot.com")
+    client.publish("test/ping", json.dumps(devices_list))
+    client.disconnect()
+
+def main():
+    networks = ["172.16.21.0/24"]
+    while True:
+        all_devices = []
+        for network in networks:
+            devices = scan_network(network)
+            all_devices.extend(devices)
+        publish_mqtt(all_devices)
+        print("Varredura concluída. Esperando 1 minuto para a próxima varredura...")
+        time.sleep(60)  # Espera 1 minuto antes de executar a próxima varredura
+
+if __name__ == "__main__":
+    main()
