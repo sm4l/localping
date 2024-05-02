db5af110868de25f3300a328c34f55ad03fb4884
Eng Lucas Mendes <86418689+sm4l@users.noreply.github.com>
2024-05-02 15:17:37 -0300

    Update localping.py

--- localping.py
+++ localping.py
@@ -2,6 +2,7 @@ import scapy.all as scapy
 import json
 import paho.mqtt.client as mqtt
 import time
+import datetime
 
 def generate_ip_list(network):
     ip_list = []
@@ -10,36 +11,41 @@ def generate_ip_list(network):
         ip_list.append(network_prefix + "." + str(i))
     return ip_list
 
-def scan_network(network):
-    arp_request = scapy.ARP(pdst=network)
+def scan_ip(ip):
+    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
 
-    devices_list = []
-    for element in answered_list:
-        success = 1
-        device_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc, "success": success}
-        devices_list.append(device_dict)
-    
-    return devices_list
+    device_dict = {}
+    if answered_list:
+        device_dict = {"ip": ip, "mac": answered_list[0][1].hwsrc, "success": 1}
+    else:
+        device_dict = {"ip": ip, "mac": "Unknown", "success": 0}
+
+    return device_dict
 
 def publish_mqtt(devices_list):
     client = mqtt.Client()
     client.connect("aerisiot.com")
-    client.publish("test/ping", json.dumps(devices_list))
+    client.publish("test/ping", json.dumps([device for device in devices_list if device["success"] == 1]))
     client.disconnect()
+
 
 def main():
     networks = ["172.16.20.0/21", "172.16.21.0/21"]  # Adicionando ambas as redes
     while True:
         all_devices = []
         for network in networks:
-            devices = scan_network(network)
-            all_devices.extend(devices)
+            ip_list = generate_ip_list(network)
+            for ip in ip_list:
+                device = scan_ip(ip)
+                all_devices.append(device)
         publish_mqtt(all_devices)
-        print("Varredura concluída. Esperando 1 minuto para a próxima varredura...")
+        agora = datetime.datetime.now()
+        print(str(agora)+ " Varredura concluída. Esperando 1 minuto para a próxima varredura...")
         time.sleep(60)  # Espera 1 minuto antes de executar a próxima varredura
 
 if __name__ == "__main__":
     main()
+
